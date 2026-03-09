e780039e6d8a75c4ac67f8640b6e89d5360c0b63
Руслан Еремеев <ruslaneremeev@MacBook-Air-Ruslan.local>
2026-03-09 18:29:32 +0300

    delete build-logic/build

--- build-logic/build/kotlin-dsl/plugins-blocks/extracted/android-compose-library.gradle.kts
+++
@@ -1,8 +0,0 @@
-                                 
-                                                      
-
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
-}
