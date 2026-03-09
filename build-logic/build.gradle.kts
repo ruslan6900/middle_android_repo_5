66bca7772d759f337037d26969e0da50b8ee482d
Руслан Еремеев <ruslaneremeev@MacBook-Air-Ruslan.local>
2026-03-09 18:24:55 +0300

    homework

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.compose.gradlePlugin)
+}
---
+++ build-logic/build/kotlin-dsl/plugins-blocks/extracted/android-compose-library.gradle.kts
@@ -0,0 +1,8 @@
+                                 
+                                                      
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
